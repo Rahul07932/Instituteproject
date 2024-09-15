470f0e9d4fefd104d315f55b4f7ef802bbef530d
RAHUL YADAV <kumarrahulyd12@gmail.com>
2024-09-15 13:01:12 +0530

    Add changes

--- Instituteproject/settings.py
+++ Instituteproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-xruqs+p1qbow1ow%xry%5o)vzix-)$56vn6)t7liwiz!*vmzwl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
