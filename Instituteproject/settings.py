dad671f8677577a8136e901bf2b12d60037b94fb
RAHUL YADAV <kumarrahulyd12@gmail.com>
2024-09-15 13:22:57 +0530

    Again Changes

--- Instituteproject/settings.py
+++ Instituteproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xruqs+p1qbow1ow%xry%5o)vzix-)$56vn6)t7liwiz!*vmzwl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
